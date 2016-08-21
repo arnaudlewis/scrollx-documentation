e6f6a50cd14f802ef82bab7c2be28cf7a51a6c00
arnaudlewis <arnaud.lewis@gmail.com>
2016-08-21 21:21:05 +0200

    add prismic config

---
+++ app/conf/prismic-configuration.js
@@ -0,0 +1,11 @@
+export default {
+
+  apiEndpoint: 'https://scrollx.prismic.io/api',
+
+  // -- Access token if the Master is not open
+  accessToken: 'MC5WN2lybGlnQUFCNGJiVXZN.ZxA3A--_vWHvv71F77-977-977-977-9fAIE77-977-9ZO-_ve-_ve-_vXHvv71w77-9NDxjdQXvv73vv70',
+
+  // OAuth
+  clientId: 'V7irligAACkAbUvL',
+  clientSecret: 'c4f07ba500c3cac29248fbcd7f53d658'
+}
